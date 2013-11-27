2bc25cca796b294b8b102e3debb84d4206abfd1d
Lihao Liang <lihao.liang@cs.ox.ac.uk>
2013-11-27 17:07:19 +0000

    <logger>: More fixes.

--- temp-logger/logger_conc.c
+++ temp-logger/logger_conc.c
@@ -76,39 +76,39 @@ void start(uint32_t _startTime,uint16_t _interval)
 
 extern uint16_t read_sensor_value();
 void task_measure(void) {
-__TRANS_enter_thread(0);
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_enter_thread(0);
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
 __VERIFIER_atomic_begin();
   tickCounter++;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
 
   if (tickCounter == interval) {
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
 __VERIFIER_atomic_begin();
     uint16_t value = read_sensor_value();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
  
 __VERIFIER_atomic_begin();
     tickCounter = 0;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
 __VERIFIER_atomic_begin();
     intervalCounter++;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
     
 __VERIFIER_atomic_begin();
     records[numberOfRecords] =  value;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
 __VERIFIER_atomic_begin();
     numberOfRecords++;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_measure();
+__VERIFIER_atomic_TRANS_atomic_assume_task_measure();
   }
-__TRANS_leave_thread(0);
+__VERIFIER_atomic_TRANS_leave_thread(0);
 }
 
 extern uint8_t read_cmd();
@@ -127,141 +127,141 @@ extern uint16_t get_uint16();
 extern uint32_t get_uint32();
 extern void send_response();
 void task_communicate(void) {
-__TRANS_enter_thread(1);
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_enter_thread(1);
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
   uint8_t decodingStatus;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
   //uint8_t cmd = get_cmd(&decodingStatus);
 __VERIFIER_atomic_begin();
   uint8_t cmd = read_cmd();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
   decodingStatus = CMD_DEC_ERROR;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 
   if(cmd==CMD_STOP || cmd==CMD_READ_STATE || 
      cmd==CMD_READ_DATA || cmd==CMD_START) {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
     decodingStatus = CMD_DEC_OK;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
   }
 
 __VERIFIER_atomic_begin();
   uint8_t rspStatus = RSP_SUCCESS;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 
   if (decodingStatus != CMD_DEC_OK) {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
     rspStatus = RSP_ERROR;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
   }
   if (rspStatus == RSP_SUCCESS) {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
     switch(cmd) {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
       case CMD_STOP:   
         if (systemState != LOGGING) {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
            rspStatus = RSP_ERROR;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         } else { //stop();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           systemState = IDLE;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         }
 __VERIFIER_atomic_begin();
         send_response();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         break;
       case CMD_READ_STATE:
 __VERIFIER_atomic_begin();
         send_response();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 	break;
       case CMD_READ_DATA:
 __VERIFIER_atomic_begin();
         send_response();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 	break;
       case CMD_START:
         if (systemState != IDLE) { 
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           rspStatus = RSP_ERROR;  
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         } else {
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           uint32_t _startTime = get_uint32();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           uint16_t _interval = get_uint16();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 	  //start(_startTime,_interval);
 	  if (numberOfRecords+1 >= MAX_RECORDS) { //stop();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
             systemState = IDLE;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
           }
 __VERIFIER_atomic_begin();
           intervalCounter = 0;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           tickCounter = 0;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           numberOfRecords = 0;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           startTime = _startTime;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           interval = _interval;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           systemState = LOGGING;
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
 __VERIFIER_atomic_begin();
           assert(numberOfRecords==0);
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         }
 __VERIFIER_atomic_begin();
         send_response();
 __VERIFIER_atomic_end();
-__TRANS_atomic_assume_task_communicate();
+__VERIFIER_atomic_TRANS_atomic_assume_task_communicate();
         break;
       default: break;
     }
   }
-__TRANS_leave_thread(1);
+__VERIFIER_atomic_TRANS_leave_thread(1);
 }
 
 
